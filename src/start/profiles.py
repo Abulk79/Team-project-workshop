40a0fec65e28c9555a713c863f364bff38bc230f
vladlen32230 <vladlen32230@gmail.com>
2024-05-28 23:05:14 +0500

    isMember fix

--- src/start/profiles.py
+++ src/start/profiles.py
@@ -10,7 +10,7 @@ def team(req, name):
 
     context['members'] = members
     context['owner'] = req.user.is_authenticated and req.user.name == team.owner.name
-    context['isOwner'] = req.user.is_authenticated and req.user.name in members
+    context['isMember'] = req.user.is_authenticated and req.user.name in members
     context['team'] = team
     context['ad'] = ad
 
