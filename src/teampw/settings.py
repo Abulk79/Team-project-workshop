a65a9531b13ae4fc60c4db6501bbd8ecdb662de6
vladlen32230 <vladlen32230@gmail.com>
2024-06-12 14:10:11 +0300

    remove database from git repo

--- src/teampw/settings.py
+++ src/teampw/settings.py
@@ -52,7 +52,7 @@ WSGI_APPLICATION = 'teampw.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
+        'NAME': Path('/database/db.sqlite3'),
     }
 }
 
